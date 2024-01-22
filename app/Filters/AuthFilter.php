9560c4d647406f74288f1129f666bc18a41ed1c2
PhamNguyenThanhTrung <trunghhf@gmail.com>
2024-01-22 11:25:32 +0700

    update

--- app/Filters/AuthFilter.php
+++ app/Filters/AuthFilter.php
@@ -11,22 +11,19 @@ class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        // Danh sách các trang không yêu cầu đăng nhập
+
         $publicPages = ['login'];
         $service = service('router');
 
         if (!session()->has('user') && !in_array($service->methodName(), $publicPages)) {
             return redirect()->to('/login');
         }
-        // Kiểm tra nếu người dùng chưa đăng nhập và trang hiện tại không thuộc danh sách trang không yêu cầu đăng nhập
-//        if (!session()->has('user') && !in_array($request->uri->getSegment(1), $publicPages)) {
-//            return redirect()->to('/login');
-//        }
+
     }
 
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        // Không cần xử lý sau khi điều hướng
+
     }
 }
